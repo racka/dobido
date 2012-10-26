9775c72050b27c115ef3cae8e318517b9b1c020e
George Gassama <gyorgy.gassama@webvalto.hu>
2012-10-26 15:36:49 +0200

    changes yet to come...

--- src/main/java/hu/nooon/dobido/server/rpc/DriveServletImpl.java
+++ src/main/java/hu/nooon/dobido/server/rpc/DriveServletImpl.java
@@ -1,30 +1,33 @@
 package hu.nooon.dobido.server.rpc;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.services.drive.Drive;
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import hu.nooon.dobido.client.rpc.DriveServlet;
-import hu.nooon.dobido.server.drive.MyGoogleDrive;
+import hu.nooon.dobido.server.drive.CredentialMediator;
+import hu.nooon.dobido.server.drive.DrEditServlet;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DriveServletImpl extends RemoteServiceServlet implements DriveServlet {
-
-    public static final String CLIENT_SECRETS_FILE_PATH
-            = "/WEB-INF/client_secrets.json";
+public class DriveServletImpl extends DrEditServlet implements DriveServlet {
 
 
     @Override
     public String listFiles(String user) {
 
         try {
-            Credential credential = MyGoogleDrive.buildEmptyCredential(
-                    getServletContext().getResourceAsStream(CLIENT_SECRETS_FILE_PATH)
-            );
-            Drive drive = MyGoogleDrive.buildService((GoogleCredential) credential);
+            HttpServletRequest request = getThreadLocalRequest();
+            request.getSession().setAttribute(CredentialMediator.USER_ID_KEY, "74120609504.apps.googleusercontent.com");
+            request.getSession().setAttribute(CredentialMediator.EMAIL_KEY, "74120609504@developer.gserviceaccount.com");
 
-            return MyGoogleDrive.getFileNames(drive);
+
+            Drive drive = getDriveService(getThreadLocalRequest(), getThreadLocalResponse());
+            return getFileNames(drive);
 
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
@@ -32,4 +35,66 @@ public class DriveServletImpl extends RemoteServiceServlet implements DriveServl
 
         return "...";
     }
+
+    /**
+     * Build and return a Drive service object based on given request parameters.
+     *
+     * @param req Request to use to fetch code parameter or accessToken session
+     *            attribute.
+     * @param resp HTTP response to use for redirecting for authorization if
+     *             needed.
+     * @return Drive service object that is ready to make requests, or null if
+     *         there was a problem.
+     */
+    private Drive getDriveService(HttpServletRequest req,
+                                  HttpServletResponse resp) {
+        Credential credentials = getCredential(req, resp);
+
+        return new Drive.Builder(TRANSPORT, JSON_FACTORY, credentials).build();
+    }
+
+
+    private String getFileNames(Drive service) throws IOException {
+
+        List<File> files = retrieveAllFiles(service);
+        if (files.isEmpty()) {
+            return "No info";
+        }
+
+        StringBuilder result = new StringBuilder("");
+
+        for (File file : files) {
+            result.append(file.getOriginalFilename());
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * Retrieve a list of File resources.
+     *
+     * @param service Drive API service instance.
+     * @return List of File resources.
+     */
+    private static List<File> retrieveAllFiles(Drive service) throws IOException {
+        List<File> result = new ArrayList<File>();
+        Drive.Files.List request = service.files().list();
+
+        do {
+            try {
+                FileList files = request.execute();
+
+                result.addAll(files.getItems());
+                request.setPageToken(files.getNextPageToken());
+            } catch (IOException e) {
+                System.out.println("An error occurred: " + e);
+                request.setPageToken(null);
+            }
+        } while (request.getPageToken() != null &&
+                request.getPageToken().length() > 0);
+
+        return result;
+    }
+
+
 }
